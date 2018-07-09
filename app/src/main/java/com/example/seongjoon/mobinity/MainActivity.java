3c821b007cfce09aed701fa3f9ac09f4953b1124
SeongjoonJeong <jdd01299@naver.com>
2018-07-10 01:09:56 +0900

    Applying the present location
    fusedLocationProvider 사용하여 내 위치 정보 얻고, 변경된 위치 지도에 적용.

--- app/src/main/java/com/example/seongjoon/mobinity/MainActivity.java
+++ app/src/main/java/com/example/seongjoon/mobinity/MainActivity.java
@@ -1,20 +1,76 @@
 package com.example.seongjoon.mobinity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_USED_PERMISSION = 200;
+    private static final String[] needPermissions = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
     private static final String MAP_BUNDLE_KEY = "MapBundleKey";
+    private static final LatLng DEFAULT_LOCATION = new LatLng(37.56641923090, 126.9778741551);
+    private static final int DEFAULT_ZOOM = 15;
+    private GoogleMap map;
     private MapView mapView;
+    private Location lastKnownLocation;
+
+    // For requesting the runtime permissions.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean permissionToLocationAccepted = true;
+
+        switch(requestCode){
+            case REQUEST_USED_PERMISSION:
+                for(int result : grantResults){
+                    if(result != PackageManager.PERMISSION_GRANTED){
+                        permissionToLocationAccepted = false;
+                        break;
+                    }
+                }
+                break;
+        }
+
+        if(permissionToLocationAccepted == false){
+            finish();
+        } else {
+            getMyLocation();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Check the requested permissions.
+        for(String permission : needPermissions){
+            if(ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,needPermissions, REQUEST_USED_PERMISSION);
+                break;
+            }
+        }
+
+        // This Bundle object (=mapViewBundle) is for updating the MapView widget whenever the state of this Activity is changed.
         Bundle mapViewBundle = null;
         if(savedInstanceState != null){
             mapViewBundle = savedInstanceState.getBundle(MAP_BUNDLE_KEY);
@@ -22,6 +78,53 @@ public class MainActivity extends AppCompatActivity {
 
         mapView = (MapView)findViewById(R.id.map);
         mapView.onCreate(mapViewBundle);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                getMyLocation();
+            }
+        });
+    }
+
+    private void getMyLocation() {
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+           && ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+
+        FusedLocationProviderClient fusedLocationProviderClient = new FusedLocationProviderClient(this);
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                lastKnownLocation = location;
+
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                   ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, DEFAULT_ZOOM));
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        });
     }
 
     @Override
