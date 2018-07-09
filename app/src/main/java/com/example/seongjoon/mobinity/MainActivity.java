08ebc8248d04132c34bcd507e261865cc3654d30
SeongjoonJeong <jdd01299@naver.com>
2018-07-09 22:41:35 +0900

    Maps API & MapView

--- app/src/main/java/com/example/seongjoon/mobinity/MainActivity.java
+++ app/src/main/java/com/example/seongjoon/mobinity/MainActivity.java
@@ -3,11 +3,74 @@ package com.example.seongjoon.mobinity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAP_BUNDLE_KEY = "MapBundleKey";
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle mapViewBundle = null;
+        if(savedInstanceState != null){
+            mapViewBundle = savedInstanceState.getBundle(MAP_BUNDLE_KEY);
+        }
+
+        mapView = (MapView)findViewById(R.id.map);
+        mapView.onCreate(mapViewBundle);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        Bundle mapBundle = outState.getBundle(MAP_BUNDLE_KEY);
+
+        if(mapBundle == null){
+            mapBundle = new Bundle();
+            outState.putBundle(MAP_BUNDLE_KEY, mapBundle);
+        }
+
+        mapView.onSaveInstanceState(mapBundle);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause(){
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy(){
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
