1ad6da7467daadd51ef11197c5cf02400cedd90c
SeongjoonJeong <jdd01299@naver.com>
2018-07-27 17:30:50 +0900

    Edit a MapActivity
    - Permission 첫 번째 page에서 처리하도록 함.
    - 맵(메인 페이지)에 Navi 메뉴 적용.

--- app/src/main/java/com/example/seongjoon/mobinity/MainActivity.java
+++ app/src/main/java/com/example/seongjoon/mobinity/MainActivity.java
@@ -1,12 +1,48 @@
 package com.example.seongjoon.mobinity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_USED_PERMISSION = 200;
+    private static final String[] needPermissions = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    // For requesting the runtime permissions.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean permissionToLocationAccepted = true;
+
+        switch(requestCode){
+            case REQUEST_USED_PERMISSION:
+                for(int result : grantResults){
+                    if(result != PackageManager.PERMISSION_GRANTED){
+                        permissionToLocationAccepted = false;
+                        break;
+                    }
+                }
+                break;
+        }
+
+        if(!permissionToLocationAccepted){
+            finish();
+        } else {
+            // 앱 자동으로 넘기게 하면 될 듯.
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); // Move the next page.
             }
         });
+
+        // Check the requested permissions.
+        for(String permission : needPermissions){
+            if(ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,needPermissions, REQUEST_USED_PERMISSION);
+                break;
+            }
+        }
     }
 
 }
